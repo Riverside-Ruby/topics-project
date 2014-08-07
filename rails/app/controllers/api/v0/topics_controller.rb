ee0729b16aa22b4ffafb3f492f349282b1484bed
Judd Lillestrand <juddl333@gmail.com>
2014-08-06 19:51:47 -0700

    add index action to topics, add topics route, add topics model

--- rails/app/controllers/api/v0/topics_controller.rb
+++ rails/app/controllers/api/v0/topics_controller.rb
@@ -1,2 +1,8 @@
 class Api::V0::TopicsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Topic.all
+  end
+
 end
--- rails/spec/controllers/api/v0/topics_spec.rb
+++ rails/spec/controllers/api/v0/topics_spec.rb
@@ -4,10 +4,18 @@ describe Api::V0::TopicsController do
   describe '#index' do
     let(:json) { ActiveSupport::JSON.decode(response.body) }
 
-    before { get :index, :format => :json }
+    before do 
+      Topic.create(:title => 'Sample Title', :description => 'Sample Description')
+      Topic.create(:title => 'Super Cool Stuff', :description => 'Sample Description')
+      get :index, :format => :json 
+    end
 
     it "should receive an :ok HTTP response from the server when calling /api/v0/topics" do
       expect(response.response_code).to equal(200)
+    end
+
+    specify "returns all topics" do
+      expect(json.count).to eq(2)
     end
   end
 end
